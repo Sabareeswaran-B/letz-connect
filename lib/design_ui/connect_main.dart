3890e35747c403c8ad2ee3370dd9f7a58a102014
Sabareeswaran B <sabareeswaran2510@gmail.com>
2021-09-27 10:42:43 +0530

    main page changes

--- lib/design_ui/connect_main.dart
+++ lib/design_ui/connect_main.dart
@@ -32,7 +32,8 @@ class _ConnectMainState extends State<ConnectMain> {
       "created_At": "July 24, 2021",
       "message": {
         "title": "selected as an intern",
-        "description": "I'm really excited to work for Smartchainers pvt, starting from next week.",
+        "description":
+            "I'm really excited to work for Smartchainers pvt, starting from next week.",
       }
     },
     {
@@ -42,10 +43,115 @@ class _ConnectMainState extends State<ConnectMain> {
       "created_At": "August 15, 2020",
       "message": {
         "title": "MCA",
-        "description": "Joined MCA in RVS college one of the best college in Coimbatore also where i did my UG.",
+        "description":
+            "Joined MCA in RVS college one of the best college in Coimbatore also where i did my UG.",
       }
     },
   ];
+
+  Widget postWidget(post) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      constraints: BoxConstraints(
+        minHeight: 180,
+      ),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              blurRadius: 2.0,
+              spreadRadius: 2.0,
+            )
+          ]),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(45),
+                    child: CachedNetworkImage(
+                      imageUrl: post['imageUrl'],
+                      placeholder: (context, url) =>
+                          CupertinoActivityIndicator(),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      // padding: EdgeInsets.only(top: 5),
+                      child: Text(
+                        post['username'],
+                        style: TextStyle(
+                            color: Colors.black87, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 5),
+                      child: Text(
+                        post['created_At'],
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              padding: EdgeInsets.only(top: 5),
+              child: Text(
+                post['message']['title'],
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              padding: EdgeInsets.only(top: 5),
+              child: Text(
+                post['message']['description'],
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,116 +172,8 @@ class _ConnectMainState extends State<ConnectMain> {
             title: Text("Letz Connect"),
           ),
           body: SingleChildScrollView(
-            child: Column(
-                children: data
-                    .map(
-                      (post) => Container(
-                        margin:
-                            EdgeInsets.fromLTRB(15, 15, 15, 0),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                        constraints: BoxConstraints(
-                          minHeight: 180,
-                        ),
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.shade300,
-                              blurRadius: 2.0,
-                              spreadRadius: 2.0,
-                            )
-                          ]
-                        ),
-                        child: Column(
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Container(
-                                    height: 40,
-                                    width: 40,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(45),
-                                      child: CachedNetworkImage(
-                                        imageUrl: post['imageUrl'],
-                                        placeholder: (context, url) =>
-                                            CupertinoActivityIndicator(),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(width: 10),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Container(
-                                        // padding: EdgeInsets.only(top: 5),
-                                        child: Text(
-                                          post['username'],
-                                          style: TextStyle(
-                                              color: Colors.black87,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                    ),
-                                    Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Container(
-                                        padding: EdgeInsets.only(top: 5),
-                                        child: Text(
-                                          post['created_At'],
-                                          style: TextStyle(
-                                              color: Colors.grey,
-                                              fontSize: 10,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 20),
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Container(
-                                padding: EdgeInsets.only(top: 5),
-                                child: Text(
-                                  post['message']['title'],
-                                  style: TextStyle(
-                                      color: Colors.black87,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Container(
-                                padding: EdgeInsets.only(top: 5),
-                                child: Text(
-                                  post['message']['description'],
-                                  style: TextStyle(
-                                      color: Colors.black54,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                    .toList()),
+            child:
+                Column(children: data.map((post) => postWidget(post)).toList()),
           )),
     );
   }
