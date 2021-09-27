d1557c295becf2a87f1037a1a1e40e5b69705ee3
Sabareeswaran B <sabareeswaran2510@gmail.com>
2021-09-27 15:10:56 +0530

    data seperate

---
+++ lib/data.dart
@@ -0,0 +1,81 @@
+List<Map> data = [
+    {
+      "postId": "1001",
+      "imageUrl":
+          "https://cdn.landesa.org/wp-content/uploads/default-user-image.png",
+      "username": "Sabareeswaran B",
+      "created_At": "July 25, 2021",
+      "message": {
+        "title": "Internship",
+        "description": "Started working at Smartchainers pvt. from July, 2021",
+      },
+      "attachments": {
+        "type": "",
+        "url": "",
+      }
+    },
+    {
+      "postId": "1002",
+      "imageUrl":
+          "https://cdn.landesa.org/wp-content/uploads/default-user-image.png",
+      "username": "Gopi",
+      "created_At": "July 24, 2021",
+      "message": {
+        "title": "selected as an intern",
+        "description":
+            "I'm really excited to work for Smartchainers pvt, starting from next week.",
+      },
+      "attachments": {
+        "type": "",
+        "url": "",
+      }
+    },
+    {
+      "postId": "1003",
+      "imageUrl":
+          "https://cdn.landesa.org/wp-content/uploads/default-user-image.png",
+      "username": "Sabareeswaran B",
+      "created_At": "August 15, 2020",
+      "message": {
+        "title": "MCA",
+        "description":
+            "Joined MCA in RVS college one of the best college in Coimbatore also where i did my UG.",
+      },
+      "attachments": {
+        "type": "",
+        "url": "",
+      }
+    },
+    {
+      "postId": "1004",
+      "imageUrl":
+          "https://cdn.landesa.org/wp-content/uploads/default-user-image.png",
+      "username": "Stalin",
+      "created_At": "August 14, 2021",
+      "message": {
+        "title": "Joining PG",
+        "description":
+            "At last got a seat in a prestigious college in Coiambatore. Exited to be a part of Rvs collage.",
+      },
+      "attachments": {
+        "type": "",
+        "url": "",
+      }
+    },
+    {
+      "postId": "1003",
+      "imageUrl":
+          "https://cdn.landesa.org/wp-content/uploads/default-user-image.png",
+      "username": "Sabareeswaran B",
+      "created_At": "October 17, 2020",
+      "message": {
+        "title": "Aws in Coursera",
+        "description":
+            "Just finished my course Aws: Building serverless applications and got my certificate.",
+      },
+      "attachments": {
+        "type": "image",
+        "url": "assets/images/certificate.jpeg",
+      }
+    },
+  ];
